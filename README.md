109dca4b761ccc85a7505166697e635426d590b2
pinkipi <yarnovax@gmail.com>
2019-05-12 19:17:21 -0400

    Update for latest proxy

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-# AFKer
-A tera-proxy module that prevents you from getting back to the character select screen. 
-This has the side effect that the server's AFK check will not kick you out of the game.  
-  
 ## Usage
-While ingame, type `/proxy afk` into chat to toggle the script on/off.
+While ingame, type `/!afk` into chat to toggle the script on/off.
--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const Command = require('command')
-
-module.exports = function AFKer(dispatch) {
-	const command = Command(dispatch)
+module.exports = function AFKer(mod) {
+	const {command} = mod.require
 
 	let enabled = false
 
-	dispatch.hook('C_RETURN_TO_LOBBY', 'raw', () => !enabled && undefined) // Prevents you from being automatically logged out while AFK
+	command.add('afk', () => { command.message(`AFKer ${(enabled = !enabled) ? 'enabled' : 'disabled'}.`) })
 
-	command.add('afk', () => {
-		enabled = !enabled
-		command.message('AFKer ' + (enabled ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>') + '.')
-	})
+	mod.hook('C_RETURN_TO_LOBBY', 'raw', () => !enabled && undefined)
 }
